f9985ce8bc6ab4d516c946c3296adc2f380c52e1
pmall <pierre.mallinjoud@gmail.com>
2025-11-19 14:47:06 +0100

    fix: add serverExternalPackages to resolve build issues with pino and thread-stream

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["pino", "thread-stream"],
 };
 
 export default withPayload(nextConfig);
